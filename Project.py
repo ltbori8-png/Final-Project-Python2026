7886082034c5ed474a077c2904b2f4de7e138064
zlblack <ltbori8@gmail.com>
2026-05-05 12:34:05 -0400

    Code now opens website... I think

--- Project.py
+++ Project.py
@@ -31,7 +31,6 @@ class PriceEntry:
 
 options = webdriver.EdgeOptions()
 options.add_experimental_option("detach", True)
-options.add_argument("headless")
 driver = webdriver.Edge(options=options)
 driver.get("https://www.amazon.com/Pokemon-TCG-Scarlet-Violet-Prismatic-Evolutions/dp/B0DLPL7LC5/ref=asc_df_B0DLPL7LC5?tag=bingshoppinga-20&linkCode=df0&hvadid=80883033960730&hvnetw=o&hvqmt=e&hvbmt=be&hvdev=c&hvlocint=&hvlocphy=95351&hvtargid=pla-4584482509038924&psc=1&msclkid=b87c67c97e491f0c1b610e336bac0581")
 print(driver.title)
