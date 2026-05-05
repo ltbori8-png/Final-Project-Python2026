1229a5e7dd9fcc4b024c6e50c71f8e1df7a71ad7
zlblack <ltbori8@gmail.com>
2026-05-05 11:54:08 -0400

    wrote json

--- Project.py
+++ Project.py
@@ -1,9 +1,12 @@
 import selenium
-import By
-import WebDriverWait
-import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import json
-import date
+from datetime import date
 
 def write_json(data):
-    with open('price.json', 'w'):
+    with open('price.json', 'w') as file:
+        json.dump(data, file, indent=4)
